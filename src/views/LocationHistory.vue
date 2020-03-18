ecf4e4da04475cf9039fa583aa737b96656af907
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-03-18 22:04:09 +0100

    update logic for laod location, add Upload processing

--- src/views/LocationHistory.vue
+++ src/views/LocationHistory.vue
@@ -5,28 +5,35 @@
                 <v-icon color="success" large>mdi-cloud-check</v-icon>
             </v-col>
             <v-col>
-                <span class="title">
-                    Nahrávání úspěšné, děkujeme!
-                </span>
-                <br /><br />
-                Pro vaše data byl přiřazen následující identifikátor. Pro
-                úspěšné dokončení ho odešlete e-mailem nebo sdělte pracovníkovi
-                hygienické stanice:
-                <br /><br />
-                <div>
-                    <span class="display-1 mr-6" style="vertical-align: middle">
-                        {{ id }}
-                    </span>
-                    <v-btn
-                        class="success"
-                        x-large
-                        elevation="0"
-                        :href="mailto"
-                        large
-                    >
-                        <v-icon class="mr-2">mdi-email</v-icon>
-                        Odeslat e-mailem
-                    </v-btn>
+                <!-- todo FE better structure -->
+                <div v-if="isProccesing">
+                    <Uploading :status="UploadStatus.PROCESSING" />
+                </div>
+                <div v-else>
+                    <span class="title">Nahrávání úspěšné, děkujeme!</span>
+                    <br />
+                    <br />Pro vaše data byl přiřazen následující identifikátor.
+                    Pro úspěšné dokončení ho odešlete e-mailem nebo sdělte
+                    pracovníkovi hygienické stanice:
+                    <br />
+                    <br />
+                    <div>
+                        <span
+                            class="display-1 mr-6"
+                            style="vertical-align: middle"
+                            >{{ id }}</span
+                        >
+                        <v-btn
+                            class="success"
+                            x-large
+                            elevation="0"
+                            :href="mailto"
+                            large
+                        >
+                            <v-icon class="mr-2">mdi-email</v-icon>Odeslat
+                            e-mailem
+                        </v-btn>
+                    </div>
                 </div>
             </v-col>
         </v-row>
@@ -62,16 +69,20 @@ import Vue from "vue";
 import Component from "vue-class-component";
 import LocationHistoryMap from "@/components/LocationHistoryMap.vue";
 import LocationHistorySidePanel from "@/components/LocationHistorySidePanel.vue";
+import Uploading from "./Uploading.vue";
 import { Location } from "@/types/Location";
 import axios from "axios";
+import { UploadStatus } from "@/types/UploadStatus";
 
 @Component({
-    components: { LocationHistorySidePanel, LocationHistoryMap }
+    components: { LocationHistorySidePanel, LocationHistoryMap, Uploading }
 })
 export default class LocationHistory extends Vue {
     id = "";
     token = "";
     mailto = "";
+    isProccesing = true;
+    UploadStatus = UploadStatus;
 
     locations: Location[] = [];
     selectedLocations: Location[] = [];
@@ -82,11 +93,34 @@ export default class LocationHistory extends Vue {
         this.id = this.$route.params.id;
         this.token = this.$route.query.token as string;
         this.mailto = `mailto:test@hygiena.cz?subject=Data pro ${this.id}`;
-
-        this.loadLocations();
+        if (!this.loadFakeLocations()) {
+            this.loadLocations();
+        }
     }
 
     async loadLocations() {
+        const url = `${process.env.VUE_APP_API_URL}/users/${this.id}/locations`;
+        const params = { token: this.token };
+
+        try {
+            const response = await axios.get(url, { params });
+            const locations = response.data;
+            console.log(locations);
+            this.isProccesing = response.status === 204;
+            if (this.isProccesing) {
+                console.log("try laod locations");
+                await setTimeout(() => this.loadLocations(), 10 * 1000);
+            } else {
+                this.locations = response.data;
+            }
+        } catch (error) {
+            this.isProccesing = false;
+            console.error({ error });
+            //todo FE error modal
+        }
+    }
+
+    loadFakeLocations(): boolean {
         if (this.id === "localhost") {
             this.locations = [
                 {
@@ -102,13 +136,9 @@ export default class LocationHistory extends Vue {
                     accuracy: 1033
                 }
             ];
-            return;
+            return true;
         }
-
-        const url = `${process.env.VUE_APP_API_URL}/users/${this.id}/locations`;
-        const params = { token: this.token };
-        const response = await axios.get(url, { params });
-        this.locations = response.data;
+        return false;
     }
 
     filterLocations(date: string) {
