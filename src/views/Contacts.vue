e31fdc7c39fe7aafb42dd4a0eb12442bf2e815fb
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-03-19 10:22:13 +0100

    Add contacts page

---
+++ src/views/Contacts.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="pa-3">
+        <h1>Kontakty</h1>
+    </div>
+</template>
+
+<style scoped></style>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({})
+export default class Contacts extends Vue {}
+</script>
