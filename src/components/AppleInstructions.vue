00c39703bb23874ba31d57e6aa83e7569d8a5fe2
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-03-19 10:28:51 +0100

    add apple instruction component

---
+++ src/components/AppleInstructions.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <span>Apple intructions</span>
+    </div>
+</template>
+
+<style></style>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({})
+export default class AppleInstructions extends Vue {}
+</script>
