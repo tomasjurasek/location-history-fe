78ce7a7de1a0d9fbc86a51e61d148bd4df9e3d36
Richard Jedlička <jedlicka.r@gmail.com>
2020-03-20 12:38:55 +0100

    apple instructions design update

--- src/views/AppleInstructions.vue
+++ src/views/AppleInstructions.vue
@@ -22,135 +22,143 @@
                     <v-col>
                         <h1>Jak na to?</h1>
 
-                        <section class="step">
-                            <header class="step__header">
-                                <div class="step__number">1.</div>
-                                <div>
-                                    <h2 class="step__title">
-                                        Zvolte <strong>Nastavení</strong>
-                                        <br />na základní obrazovce
-                                    </h2>
-                                </div>
-                            </header>
-                            <img
-                                class="step__image"
-                                src="instructions/iphone/iphone-step-1.png"
-                            />
-                        </section>
+                        <div class="steps row">
+                            <section class="step col-lg-4-bak col-sm-6-bak">
+                                <header class="step__header">
+                                    <div class="step__number">1.</div>
+                                    <div>
+                                        <h2 class="step__title">
+                                            Zvolte <strong>Nastavení</strong>
+                                        </h2>
+                                        <p class="step__description">
+                                            na základní obrazovce
+                                        </p>
+                                    </div>
+                                </header>
+                                <img
+                                    class="step__image"
+                                    src="instructions/iphone/iphone-step-1.jpg"
+                                />
+                            </section>
 
-                        <section class="step">
-                            <header class="step__header">
-                                <div class="step__number">2.</div>
-                                <div>
-                                    <h2 class="step__title">
-                                        Zvolte
-                                        <strong>Soukromí</strong>
-                                    </h2>
-                                    <p class="step__description">
-                                        v menu Nastavení
-                                    </p>
-                                </div>
-                            </header>
-                            <img
-                                class="step__image"
-                                src="instructions/iphone/iphone-step-2.png"
-                            />
-                        </section>
+                            <section class="step col-lg-4-bak col-sm-6-bak">
+                                <header class="step__header">
+                                    <div class="step__number">2.</div>
+                                    <div>
+                                        <h2 class="step__title">
+                                            Zvolte
+                                            <strong>Soukromí</strong>
+                                        </h2>
+                                        <p class="step__description">
+                                            v menu Nastavení
+                                        </p>
+                                    </div>
+                                </header>
+                                <img
+                                    class="step__image"
+                                    src="instructions/iphone/iphone-step-2.jpg"
+                                />
+                            </section>
 
-                        <section class="step">
-                            <header class="step__header">
-                                <div class="step__number">3.</div>
-                                <div>
-                                    <h2 class="step__title">
-                                        Vyberte položku
-                                        <strong>Polohové služby</strong>
-                                    </h2>
-                                    <p class="step__description">
-                                        z menu Soukromí
-                                    </p>
-                                </div>
-                            </header>
-                            <img
-                                class="step__image"
-                                src="instructions/iphone/iphone-step-3.png"
-                            />
-                        </section>
+                            <section class="step col-lg-4-bak col-sm-6-bak">
+                                <header class="step__header">
+                                    <div class="step__number">3.</div>
+                                    <div>
+                                        <h2 class="step__title">
+                                            Vyberte položku
+                                            <strong>Polohové služby</strong>
+                                        </h2>
+                                        <p class="step__description">
+                                            z menu Soukromí
+                                        </p>
+                                    </div>
+                                </header>
+                                <img
+                                    class="step__image"
+                                    src="instructions/iphone/iphone-step-3.jpg"
+                                />
+                            </section>
 
-                        <section class="step">
-                            <header class="step__header">
-                                <div class="step__number">4.</div>
-                                <div>
-                                    <h2 class="step__title">
-                                        Vyberte položku
-                                        <strong>Systémové služby</strong>
-                                    </h2>
-                                    <p class="step__description">
-                                        na konci seznamu
-                                    </p>
-                                </div>
-                            </header>
-                            <img
-                                class="step__image"
-                                src="instructions/iphone/iphone-step-4.png"
-                            />
-                        </section>
+                            <section class="step col-lg-4-bak col-sm-6-bak">
+                                <header class="step__header">
+                                    <div class="step__number">4.</div>
+                                    <div>
+                                        <h2 class="step__title">
+                                            Vyberte položku
+                                            <strong>Systémové služby</strong>
+                                        </h2>
+                                        <p class="step__description">
+                                            na konci seznamu
+                                        </p>
+                                    </div>
+                                </header>
+                                <img
+                                    class="step__image"
+                                    src="instructions/iphone/iphone-step-4.jpg"
+                                />
+                            </section>
 
-                        <section class="step">
-                            <header class="step__header">
-                                <div class="step__number">5.</div>
-                                <div>
-                                    <h2 class="step__title">
-                                        Vyberte
-                                        <strong>Významná místa</strong>
+                            <section class="step col-lg-4-bak col-sm-6-bak">
+                                <header class="step__header">
+                                    <div class="step__number">5.</div>
+                                    <div>
+                                        <h2 class="step__title">
+                                            Vyberte
+                                            <strong>Významná místa</strong>
+                                        </h2>
+                                        <p class="step__description">
+                                            v Systémových službách
+                                        </p>
+                                    </div>
+                                </header>
+                                <img
+                                    class="step__image"
+                                    src="instructions/iphone/iphone-step-5.jpg"
+                                />
+                            </section>
 
-                                        <strong>v Systémových službách</strong>
-                                    </h2>
-                                </div>
-                            </header>
-                            <img
-                                class="step__image"
-                                src="instructions/iphone/iphone-step-5.png"
-                            />
-                        </section>
+                            <section class="step col-lg-4-bak col-sm-6-bak">
+                                <header class="step__header">
+                                    <div class="step__number">6.</div>
+                                    <div>
+                                        <h2 class="step__title">
+                                            Úplně dole je sekce nazvaná
+                                            <strong>Historie</strong>
+                                        </h2>
+                                        <p class="step__description">
+                                            Vyberte ji
+                                        </p>
+                                    </div>
+                                </header>
+                                <img
+                                    class="step__image"
+                                    src="instructions/iphone/iphone-step-6.jpg"
+                                />
+                            </section>
 
-                        <section class="step">
-                            <header class="step__header">
-                                <div class="step__number">6.</div>
-                                <div>
-                                    <h2 class="step__title">
-                                        Úplně dole je sekce nazvaná
-                                        <strong>Historie</strong>
-                                    </h2>
-                                    <p class="step__description">
-                                        Vyberte ji
-                                    </p>
-                                </div>
-                            </header>
-                            <img
-                                class="step__image"
-                                src="instructions/iphone/iphone-step-6.png"
-                            />
-                        </section>
+                            <section class="step col-lg-4-bak col-sm-6-bak">
+                                <header class="step__header">
+                                    <div class="step__number">7.</div>
+                                    <div>
+                                        <h2 class="step__title">
+                                            Zobrazí se mapa spolu se seznamem
+                                            míst, které jste navštivili
+                                        </h2>
+                                        <p class="step__description">
+                                            Kliknutím na položku seznamu se
+                                            zobrazí detail místa
+                                        </p>
+                                    </div>
+                                </header>
+                                <img
+                                    class="step__image"
+                                    src="instructions/iphone/iphone-step-7.jpg"
+                                />
+                            </section>
 
-                        <section class="step">
-                            <header class="step__header">
-                                <div class="step__number">7.</div>
-                                <div>
-                                    <h2 class="step__title">
-                                        Zobrazí se mapa spolu se seznamem míst,
-                                        které jste navštivili
-                                    </h2>
-                                    <p class="step__description">
-                                        Kliknutím na položku seznamu se zobrazí
-                                        detail místa
-                                    </p>
-                                </div>
-                            </header>
-                            <img
-                                class="step__image"
-                                src="instructions/iphone/iphone-step-7.png"
-                            />
-                        </section>
+                            <div class="step-placeholder"></div>
+                            <div class="step-placeholder"></div>
+                        </div>
                     </v-col>
                 </v-row>
             </v-container>
@@ -184,30 +192,54 @@
 }
 
 h1 {
-    margin: 48px 0;
+    margin-top: 48px;
     font-size: 36px;
     font-weight: 600;
     text-align: center;
 }
 
-.step {
-    max-width: 600px;
+.steps {
+    max-width: 1440px;
     margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    /*max-width: 1300px;
+    margin: 0 auto;*/
+}
+
+.step {
+    min-height: 658px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-top: 52px;
+    margin-bottom: 32px;
+    width: 364px;
+    padding: 0 32px;
+    /*flex: 0 1 auto;
+    margin: 0 auto;*/
 }
 
 .step__header {
     display: flex;
     flex-direction: row;
-    align-items: center;
-    margin: 38px 0;
+    align-items: flex-start;
     padding: 16px 0;
+    margin-bottom: 16px;
+    margin-right: -32px;
     line-height: 28px;
 }
 
 .step__number {
     margin-right: 32px;
+    line-height: 0.75;
     font-size: 72px;
     font-weight: 600;
+}
+
+.step__header > div:nth-child(2) {
 }
 
 .step__title {
@@ -229,8 +261,32 @@ h1 {
 }
 
 .step__image {
+    display: block;
     max-width: 100%;
-    margin-top: 16px;
+    margin-top: auto;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
+}
+
+.step-placeholder {
+    width: 364px;
+}
+
+@media (max-width: 751px) {
+    .step {
+        min-height: 0;
+    }
+}
+
+@media (max-width: 400px) {
+    .step {
+        width: auto;
+        max-width: 300px;
+        padding: 0;
+    }
+
+    .step__header {
+        margin-right: 0;
+    }
 }
 </style>
 
