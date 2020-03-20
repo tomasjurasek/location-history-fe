8bc6e1aefa32b338f0ff120b85f5be73f8924749
Jan Zak <zj@zakjan.cz>
2020-03-20 19:04:49 +0100

    Phone number verification flow (WIP)

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,46 +1,78 @@
 <template>
-    <div class="form">
-        <v-file-input
-            class="file-input"
-            placeholder="Vybrat soubor"
-            accept=".zip"
-            v-model="file"
-            background-color="white"
-            hide-details
-            outlined
-        />
-        <v-btn
-            class="upload-button"
-            v-on:click="submitFile()"
-            :loading="uploading"
-            :disabled="!file"
-            color="success"
-            x-large
-            depressed
-            dark
-        >
-            Nahrát
-        </v-btn>
+    <div>
+        <v-form v-model="form1Valid" v-if="!verificationCodeSent">
+            <v-text-field
+                v-model="phoneNumber"
+                label="Telefonní číslo"
+                background-color="white"
+                filled
+                rounded
+                :rules="[phoneNumberValidation]"
+                class="mx-auto"
+                style="width: 323px"
+            />
+            <v-btn
+                color="success"
+                dark
+                x-large
+                depressed
+                :disabled="!form1Valid"
+                @click="sendVerificationCode"
+            >
+                Odeslat ověřovací SMS
+            </v-btn>
+        </v-form>
+        <v-form v-model="form2Valid" v-else>
+            <p>
+                Ověřovací SMS s kódem byla odeslána na telefonní číslo
+                {{ phoneNumber }}
+            </p>
+            <v-text-field
+                v-model="verificationCode"
+                label="Ověřovací kód z SMS"
+                background-color="white"
+                filled
+                rounded
+                :rules="[verificationCodeValidation]"
+                class="mx-auto"
+                style="width: 323px"
+            />
+            <v-file-input
+                placeholder="Vybrat soubor"
+                accept=".zip"
+                v-model="file"
+                background-color="white"
+                hide-details
+                outlined
+                :rules="[fileValidation]"
+            />
+            <v-btn
+                v-on:click="submitFile()"
+                :loading="uploading"
+                :disabled="!form2Valid"
+                color="success"
+                x-large
+                depressed
+                dark
+            >
+                Nahrát
+            </v-btn>
+        </v-form>
     </div>
 </template>
 
 <style scoped>
-.failed {
-    max-width: 600px;
-    margin-left: auto;
-    margin-right: auto;
-}
-
-.form {
+.v-form {
     text-align: center;
-}
-
-.file-input {
     max-width: 600px;
     margin: 0 auto !important;
 }
 
-.file-input /deep/ .v-input__prepend-outer {
+.v-text-field--rounded {
+    border-radius: 4px;
+}
+
+.v-file-input /deep/ .v-input__prepend-outer {
     position: absolute;
     top: 0;
     left: 16px;
@@ -48,21 +80,24 @@
     z-index: 100;
 }
 
-.file-input /deep/ .v-input__prepend-outer button {
+.v-file-input /deep/ .v-input__prepend-outer button {
     color: black;
 }
 
-.file-input /deep/ .v-input__slot {
+.v-file-input /deep/ .v-input__slot {
     padding-left: 5em !important;
 }
 
-.upload-button {
-    height: 60px !important;
-    min-width: 200px !important;
+.v-btn {
     margin-top: 20px;
 }
 
-.upload-button.theme--dark.v-btn.v-btn--disabled:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
+.v-btn:not(.v-btn--round).v-size--x-large {
+    height: 60px;
+    padding: 0 48px;
+}
+
+.v-btn.v-btn.v-btn--disabled.theme--dark:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
     background-color: #777777 !important;
     color: white !important;
 }
@@ -74,12 +109,41 @@ import Component from "vue-class-component";
 
 @Component({})
 export default class UploadForm extends Vue {
-    uploading = false;
+    phoneNumber = "+420";
+    form1Valid = false;
+
+    verificationCodeSent = false;
+
+    verificationCode = "";
     file: File | null = null;
+    form2Valid = false;
+
+    uploading = false;
+
+    sendVerificationCode() {
+        // TODO: send verification code to phone number
+        this.verificationCodeSent = true;
+    }
 
     submitFile() {
         this.uploading = true;
+        // TODO: send verification code with file upload
         this.$emit("uploadFileEvent", this.file);
+    }
+
+    phoneNumberValidation(value: string) {
+        return !!value.match(/^\+420\d{9}$/); // TODO: international phone numbers
+    }
+
+    verificationCodeValidation(value: string) {
+        return !!value.match(/^\d{6}$/); // TODO: match with real verification code format
+    }
+
+    fileValidation(value: File | null) {
+        return (
+            (value && value.size < 200 * 1024 * 1024) ||
+            "Maximální povolená velikost souboru je 200 MB"
+        );
     }
 }
 </script>
